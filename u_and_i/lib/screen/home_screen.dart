612147d61f65394362d0f02ad86489171339c918
jerry M <lwj0173@gmail.com>
2025-01-02 14:29:51 +0900

    finish

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -31,9 +33,25 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void onHeartPressed() {
-    setState((){
-      firstDay = firstDay.subtract(Duration(days:1));
-    });
+    showCupertinoDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                color: Colors.white,
+                height: 300,
+                child: CupertinoDatePicker(
+                  mode: CupertinoDatePickerMode.date,
+                  onDateTimeChanged: (DateTime date) {
+                    setState(() {
+                      firstDay=date;
+                    });
+                  },
+                )));
+      },
+      barrierDismissible: true,
+    );
   }
 }
 
