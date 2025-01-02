e96d6c282b905eceb46a8ed0986008572e7d4be7
jerry M <lwj0173@gmail.com>
2025-01-02 14:02:24 +0900

    uani

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -1,38 +1,97 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
-  const HomeScreen({Key? key}) : super(key : key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime firstDay = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink[100],
-      body: SafeArea(
-        top: true,
-        bottom: false,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            _DDay(),
-            _CoupleImage(),
-          ],
-        )
-      )
+        backgroundColor: Colors.pink[100],
+        body: SafeArea(
+            top: true,
+            bottom: false,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _DDay(
+                  onHeartPressed: onHeartPressed,
+                  firstDay: firstDay,
+                ),
+                _CoupleImage(),
+              ],
+            )));
+  }
+
+  void onHeartPressed() {
+    setState((){
+      firstDay = firstDay.subtract(Duration(days:1));
+    });
+  }
+}
+
+class _DDay extends StatelessWidget {
+  final GestureTapCallback onHeartPressed;
+  final DateTime firstDay;
+
+  _DDay({
+    required this.onHeartPressed,
+    required this.firstDay,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final now = DateTime.now();
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        Text(
+          'U and I',
+          style: textTheme.headlineLarge,
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+          'first meet',
+          style: textTheme.bodyMedium,
+        ),
+        Text('${firstDay.year}.${firstDay.month}.${firstDay.day}',
+            style: textTheme.bodySmall),
+        const SizedBox(height: 16.0),
+        IconButton(
+          iconSize: 60.0,
+          onPressed: onHeartPressed,
+          icon: Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+            'D+${DateTime(now.year, now.month, now.day).difference(firstDay).inDays + 1}',
+            style: textTheme.headlineMedium)
+      ],
     );
   }
 }
 
-class _DDay extends StatelessWidget{
+class _CoupleImage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Text('dday widget');
-  }
-}
-
-class _CoupleImage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Text('Couple image Widget');
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: Image.asset(
+          'asset/img/1.png',
+          height: MediaQuery.of(context).size.height / 2,
+        ),
+      ),
+    );
   }
 }
