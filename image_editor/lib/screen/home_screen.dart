b24451ee479aaa35f101976ee53d5d989943ba9f
jerry M <lwj0173@gmail.com>
2025-01-06 16:51:54 +0900

    풋터

--- image_editor/lib/screen/home_screen.dart
+++ image_editor/lib/screen/home_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_editor/component/main_app_bar.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image_editor/component/main_app_bar.dart';
+import 'package:image_editor/component/footer.dart';
+import 'dart:io';
+
+import '../component/footer.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,32 +15,72 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   XFile? image;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
+        body: Stack(
+      fit: StackFit.expand,
+      children: [
+        renderBody(),
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: MainAppBar(
+              onPickImage: onPickImage,
+              onSaveImage: onSaveImage,
+              onDeleteItem: onDeleteItem),
+        ),
+
+        if (image!=null)
           Positioned(
-            top: 0,
+            bottom: 0,
             left: 0,
             right: 0,
-            child: MainAppBar(onPickImage: onPickImage, onSaveImage: onSaveImage, onDeleteItem: onDeleteItem),
+            child: Footer(
+              onEmoticonTap: onEmoticonTap,
+            ),
           )
-        ],
-      )
-    );
+      ],
+    ));
   }
 
-  void onPickImage() async{
+  Widget renderBody() {
+    if (image != null) {
+      return Positioned.fill(
+        child: InteractiveViewer(
+            child: Image.file(
+          File(image!.path),
+          fit: BoxFit.cover,
+        )),
+      );
+    } else {
+      return Center(
+        child: TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.grey,
+          ),
+          onPressed: onPickImage,
+          child: Text('choose image'),
+        ),
+      );
+    }
+  }
+
+  void onEmoticonTap(int index){
+
+  }
+
+  void onPickImage() async {
     final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-    setState((){
+    setState(() {
       this.image = image;
     });
   }
-  void onSaveImage(){}
-  void onDeleteItem(){}
+
+  void onSaveImage() {}
+
+  void onDeleteItem() {}
 }
