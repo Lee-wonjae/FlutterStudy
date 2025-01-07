46f2963a32f8498dfabc2ed650bcfaff7f3bb008
jerry M <lwj0173@gmail.com>
2025-01-07 17:33:37 +0900

    dependency

--- jr_tube/lib/screen/home_screen.dart
+++ jr_tube/lib/screen/home_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:jr_tube/component/custom_youtube_player.dart';
+import 'package:jr_tube/model/video_model.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen')
-    );
+        backgroundColor: Colors.black,
+        body: CustomYoutubePlayer(
+            videoModel: VideoModel(id: '234324', title: 'sdfdfdfsdf')));
   }
 }
